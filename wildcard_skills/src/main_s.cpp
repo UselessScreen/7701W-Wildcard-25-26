68a19a2793d0fac2504648535883749b41d98146
tanishqvithun <tanishqvithun@gmail.com>
2026-01-21 14:48:14 -0500

    Added vexcode pragma configuration also renamed the file because of compile error (no parentheses allowed in filename)

---
+++ wildcard_skills/src/main_s.cpp
@@ -0,0 +1,177 @@
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include "vex.h"
+
+using namespace vex;
+using std::string;
+using std::cout;
+using std::endl;
+
+// Brain should be defined by default
+brain Brain;
+
+
+// START V5 MACROS
+#define waitUntil(condition) \
+  do                         \
+  {                          \
+    wait(5, msec);           \
+  } while (!(condition))
+
+#define repeat(iterations) \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+// Robot configuration code.
+// PORT 4 IS RESERVED ----- FORTNITE
+
+// Independent motors
+motor scoreMotor = motor(PORT10, ratio6_1, true);
+motor intakeMotor = motor(PORT3, ratio6_1, true);
+
+// Drivetrain motors
+motor leftMotorA = motor(PORT11, ratio6_1, false);
+motor leftMotorB = motor(PORT1, ratio6_1, false);
+motor leftMotorC = motor(PORT19, ratio6_1, false);
+motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB, leftMotorC);
+motor rightMotorA = motor(PORT7, ratio6_1, true);
+motor rightMotorB = motor(PORT20, ratio6_1, true);
+motor rightMotorC = motor(PORT8, ratio6_1, true);
+motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB, rightMotorC);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 299.24, 11.43, 12.953999999999999, mm, 1.3333333333333333);
+
+controller Controller1 = controller(primary);
+
+// generating and setting random seed
+void initializeRandomSeed()
+{
+  int systemTime = Brain.Timer.systemHighResolution();
+  double batteryCurrent = Brain.Battery.current();
+  double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
+
+  // Combine these values into a single integer
+  int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
+
+  // Set the seed
+  srand(seed);
+}
+
+
+
+
+void vexcodeInit()
+{
+
+  // Initializing random seed.
+  initializeRandomSeed();
+}
+
+// Helper to make playing sounds from the V5 in VEXcode easier and
+// keeps the code cleaner by making it clear what is happening.
+void playVexcodeSound(const char *soundName)
+{
+  printf("VEXPlaySound:%s\n", soundName);
+  wait(5, msec);
+}
+
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3
+      // right = Axis2
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position();
+      int drivetrainRightSideSpeed = Controller1.Axis2.position();
+      
+      // check if the value is inside of the deadband range
+      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
+        // check if the left motor has already been stopped
+        if (DrivetrainLNeedsToBeStopped_Controller1) {
+          // stop the left drive motor
+          LeftDriveSmart.stop();
+          // tell the code that the left motor has been stopped
+          DrivetrainLNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
+        DrivetrainLNeedsToBeStopped_Controller1 = true;
+      }
+      // check if the value is inside of the deadband range
+      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
+        // check if the right motor has already been stopped
+        if (DrivetrainRNeedsToBeStopped_Controller1) {
+          // stop the right drive motor
+          RightDriveSmart.stop();
+          // tell the code that the right motor has been stopped
+          DrivetrainRNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
+        DrivetrainRNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainLNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainRNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+volatile bool interrupt = false;
+
+void onLeftPressed() {
+  // Function to be called when the left button is pressed
+  interrupt = true;
+}
+
+void controlSet() {
+  // Controller setup
+  Controller1.ButtonLeft.pressed(onLeftPressed);
+}
+
+// Initialise the controller task --> Controller1
+task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+
+#pragma endregion VEXcode Generated Robot Configuration
+/* #region project info */
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main(S).cpp                                                  */
+/*    Author:       TV                                                        */
+/*    Created:      1/21/2026, 1:05:06 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+/* #endregion */
+
+
+int main() {
+
+}
